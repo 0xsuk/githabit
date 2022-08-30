a88c1036fc5d41aa9b48be080063132053b0300e
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-08-30 14:32:08 +0900

    react-heat-map setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import "react-calendar-heatmap/dist/styles.css";
+import CalendarHeatmap from "react-calendar-heatmap";
 
 function App() {
-  return <div className="App"></div>;
+  const endDate = Date.now();
+  const startDate = endDate - 1000 * 60 * 60 * 24 * 365; //substract one year
+
+  const handleYay = () => {
+    //ping
+    fetch("http://localhost:8000/");
+  };
+  return (
+    <div id="wrapper">
+      <h1>githabit</h1>
+      <div>
+        <p>Didn't smoke cigarettes</p>
+        <button onClick={handleYay}>Yay</button>
+      </div>
+      <CalendarHeatmap
+        startDate={startDate}
+        endDate={endDate}
+        values={[
+          { date: "2022-01-01", count: 12 },
+          { date: "2022-07-22", count: 122 },
+          { date: "2022-08-30", count: 38 },
+          // ...and so on
+        ]}
+        titleForValue={(value) => {
+          return value ? `(${value.date}) ${value.count}` : null;
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
